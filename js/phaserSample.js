e9e36aad8be338f10f6336ef5784a56d7ba6faf1
joohyun jo <jjh@alt-a.net>
2022-05-18 15:23:52 +0900

    feature: add wall with tilemap

--- js/phaserSample.js
+++ js/phaserSample.js
@@ -8,10 +8,7 @@ let yGridCnt = 12;
 let aspectRatio = yGridCnt/xGridCnt;
 let reverseAspectRatio = xGridCnt/yGridCnt;
 
-// let screenAspect = window.innerHeight / window.innerWidth;
-// let reverseScreenAspect = window.innerWidth / window.innerHeight;
-
-let phrHeight = window.innerHeight * 0.95;
+let phrHeight = window.innerHeight * 0.919;
 let phrWidth = phrHeight * reverseAspectRatio;
 
 if(phrWidth > window.innerWidth) {
@@ -28,13 +25,10 @@ if(phrWidth > window.innerWidth) {
 let cnvAdjHeight = (window.innerHeight - phrHeight) / 2;
 let cnvAdjWidth = (window.innerWidth - phrWidth) / 2;
 
-// let divAdjHeight = (window.innerHeight - phrHeight) / 2;
-// let divAdjWidth = 0;
-
 // 한 cell 변의 길이
 let spacer = phrHeight / yGridCnt;
 
-//자연수 좌표계[(0,0) (1,1)....]를 canvas의 좌표계에 mapping한다. [(0,0) (5.55, 23.34)....]
+//자연수 좌표계[(0,0) (1,1)....]를 canvas의 좌표계에 mapping한다. [(0,0) (19.1919, 23.34)....]
 let xCordinates = [];
 let yCordinates = [];
 
@@ -63,12 +57,12 @@ let config = {
     pixelArt: true,
     backgroundColor: '#b7dfed',
     canvasStyle: `left: ${cnvAdjWidth}px; top: ${cnvAdjHeight}px; position: fixed;`,
-    // physics: {
-    //     default: 'arcade',
-    //     arcade: {
-    //         gravity: { y: 0 }
-    //     }
-    // },
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { y: 0 }
+        }
+    },
     scene: {
         preload: preload,
         create: create
@@ -76,74 +70,79 @@ let config = {
 };
 
 let game = new Phaser.Game(config);
-let parkingLotMap = {
-    key: 'map',
-    type: 'map',
-    infinite: false,
-    orientation:"orthogonal",
-    renderorder:"right-down",
-    width: xGridCnt,
-    height: yGridCnt,
-    tileHeight: 32,
-    tileWidth: 32,
-    layers: [
-        {
-            data: [
-                20, 20, 20, 20, 20, 20, 20,
-                20, 1, 1, 1, 1, 1, 20,
-                20, 1, 1, 1, 1, 1, 20,
-                20, 1, 1, 1, 1, 1, 20,
-                20, 1, 1, 1, 1, 1, 20,
-                20, 1, 1, 1, 1, 1, 20,
-                20, 1, 1, 1, 1, 1, 20,
-                20, 1, 1, 1, 1, 1, 20,
-                20, 1, 1, 1, 1, 1, 20,
-                20, 1, 1, 1, 1, 1, 20,
-                20, 1, 1, 1, 1, 1, 20,
-                20, 20, 20, 20, 20, 20, 20
-            ],
-            id: 3,
-            name: 'level1',
-            height: yGridCnt,
-            width: xGridCnt,
-            opacity: 1,
-            type: 'tilelayer',
-            visible: true,
-            x: 0,
-            y: 0
-        }
-    ]
-};
-let tileset;
+
+// let parkingLotMap = {
+//     key: 'map',
+//     type: 'map',
+//     infinite: false,
+//     orientation:"orthogonal",
+//     renderorder:"right-down",
+//     width: xGridCnt,
+//     height: yGridCnt,
+//     tileHeight: 32,
+//     tileWidth: 32,
+//     layers: [
+//         {
+//             data: [
+//                 20, 20, 20, 20, 20, 20, 20,
+//                 20, 1, 1, 1, 1, 1, 20,
+//                 20, 1, 1, 1, 1, 1, 20,
+//                 20, 1, 1, 1, 1, 1, 20,
+//                 20, 1, 1, 1, 1, 1, 20,
+//                 20, 1, 1, 1, 1, 1, 20,
+//                 20, 1, 1, 1, 1, 1, 20,
+//                 20, 1, 1, 1, 1, 1, 20,
+//                 20, 1, 1, 1, 1, 1, 20,
+//                 20, 1, 1, 1, 1, 1, 20,
+//                 20, 1, 1, 1, 1, 1, 20,
+//                 20, 20, 20, 20, 20, 20, 20
+//             ],
+//             id: 3,
+//             name: 'level1',
+//             height: yGridCnt,
+//             width: xGridCnt,
+//             opacity: 1,
+//             type: 'tilelayer',
+//             visible: true,
+//             x: 0,
+//             y: 0
+//         }
+//     ]
+// };
 
 function preload () {
-    //this.load.setBaseURL('http://localhost:3000');
-
-    // this.load.image('sky', 'assets/skies/space3.png');
-    // this.load.image('logo', 'assets/sprites/phaser3-logo.png');
-    // this.load.image('red', 'assets/particles/red.png');
-
-    // this.load.image('tiles', 'assets/drawtiles-spaced.png');
-    // this.load.tilemapCSV('map', 'grid/tilemaps/csv/grid.csv');
 
     this.load.image('tiles', 'assets/gridtiles.png');    
-    //this.load.tilemapCSV('map', 'grid/tilemaps/csv/grid.csv');
-
 
 }
-
-
 
 function create () {
 
     console.log(spacer);
-    let map = this.make.tilemap(parkingLotMap);
-    tileset = map.addTilesetImage('tiles', 'map', 32, 32, 0, 0);
-    //let layer = map.createLayer(0, tileset, 0, 0);
+    
+    let wall = this.make.tilemap({
+        data: [
+            [19, 19, 19, 19, 19, 19, 19],
+            [19, 0, 0, 0, 0, 0, 19],
+            [19, 0, 0, 0, 0, 0, 19],
+            [19, 0, 0, 0, 0, 0, 19],
+            [19, 0, 0, 0, 0, 0, 19],
+            [19, 0, 0, 0, 0, 0, 19],
+            [19, 0, 0, 0, 0, 0, 19],
+            [19, 0, 0, 0, 0, 0, 19],
+            [19, 0, 0, 0, 0, 0, 19],
+            [19, 0, 0, 0, 0, 0, 19],
+            [19, 0, 0, 0, 0, 0, 19],
+            [19, 19, 19, 19, 19, 19, 19]
+        ],
+        tileWidth: 32,
+        tileHeight: 32,
+        width: xGridCnt,
+        height: yGridCnt
+    });
 
-    let layer = map.createLayer('level1', tileset);
-
-
-
+    let wallTileset = wall.addTilesetImage('tiles',null, 32, 32);
+    let wallLayer = wall.createLayer(0, wallTileset, 0, 0);
+    wallLayer.setScale(spacer / 32);
    
 }
